ff6a999b88b0a42dedb2a427ecd14e40cb9d7bb4
CorrineMuriuki <Corrine.Muriuki@student.moringaschool.com>
2022-10-27 11:33:26 +0300

    Completed assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+def roll_call_dwarves(dwarfs)# code an argument here
   # Your code here
+  dwarfs.each_with_index do | name, index |
+    puts "#{index +1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet array # code an argument here
   # Your code here
+  array.map{|item|"#{item.capitalize!}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(long_planeteer_calls)# code an argument here
   # Your code here
+  answer = false
+  long_planeteer_calls.each do |call|
+    if call.length > 4
+      answer = true
+    end
+  end
+    answer
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
